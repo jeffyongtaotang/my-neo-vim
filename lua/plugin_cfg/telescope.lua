b478d213710640a9d2a2c7b393f2b383de957787
jeffyongtaotang <jeff.tong@konghq.com>
2023-01-20 01:14:57 -0700

    refactor(*) improve keymap

--- lua/plugin_cfg/telescope.lua
+++ lua/plugin_cfg/telescope.lua
@@ -1,9 +1,13 @@
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
+local actions = require("telescope.actions")
+
 require('telescope').setup {
     defaults = {
       mappings = {
         i = {
+          ['<C-j>'] = actions.move_selection_next,
+          ['<C-k>'] = actions.move_selection_previous,
           ['<C-u>'] = false,
           ['<C-d>'] = false,
         },
